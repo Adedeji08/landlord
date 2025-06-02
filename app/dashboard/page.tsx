9e00ce7bbff1abf5d66a5744b51579154be4fa3e
Oluwadamilola Adedeji <ebenoluwadamilola@gmail.com>
2025-06-02 14:28:26 +0100

    test

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+
 "use client";
 import SideNavBar from "@/component/common/SideNavBar";
 import TopNavBar from "@/component/common/TopNavBar";
